1fcf81375edae989fa867565573d5a75ae5a1aaa
Israel Saeta Pérez <dukebody@gmail.com>
2011-11-19 14:32:45 +0100

    BorderPoint new implmentation.

--- BorderPoint.java
+++ BorderPoint.java
@@ -1,19 +1,35 @@
 /*
  *  Class BorderPoint
+ * 
  *  @author Sergi Sisó Gòdia
  */
 
 public class BorderPoint extends Point {
 
-private Zone zone;
+    private List<Zone> zones = new ArrayList<Zone>(2);
 
-public BorderPoint(double latitude, double longitude, Zone zone) {
-super(latitude,longitude);
-this.zone = zone;
-}
+    public BorderPoint(double latitude, double longitude) {
+        super(latitude,longitude);
+    }
 
-public Zone getZone() {
-return zone;
-}
+    public BorderPoint(double latitude, double longitude, Zone zone) {
+        super(latitude,longitude);
+        zones.add(zone);
+    }
 
+    public boolean addZone(Zone zone) {
+        return zones.add(zone);
+    }
+
+    public List<Zone> getZones() {
+        return zones;
+    }
+
+    public Iterator getZonesIterator() {
+        return zones.iterator();
+    }
+
+    public boolean removeZone(Zone zone) {
+        return zones.remove(zone);
+    }
 }
