4ff0c04757b558120c498a8a8d4809108554ceb6
Israel Saeta Pérez <dukebody@gmail.com>
2011-11-20 15:20:10 +0100

    Add Toponym class.

--- Toponym.java
+++ Toponym.java
@@ -6,28 +6,32 @@
 
 public class Toponym extends Point implements IGetId {
 
-private String nameASCII;
-private String nameUTF;
-private TypeToponym type;
-private String id;
-private static int next_id = 0;
-public Toponym(String nameASCII, String nameUTF,double latitude, double longitude, TypeToponym type) {
-super(latitude, longitude);
-this.nameASCII = nameASCII;
-this.nameUTF = nameUTF;
-this.type = type;
-this.id = ((Integer)next_id++).toString();
-}
-public String getNameASCII() {
-return nameASCII;
-}
-public String getNameUTF() {
-return nameUTF;
-}
-public TypeToponym getType() {
-return type;
-}
-public String getId() {
-return id;
-}
+    private String nameASCII;
+    private String nameUTF;
+    private TypeToponym type;
+    private String id;
+    private static int next_id = 0;
+    public Toponym(String nameASCII, String nameUTF,double latitude, double longitude, TypeToponym type) {
+        super(latitude, longitude);
+        this.nameASCII = nameASCII;
+        this.nameUTF = nameUTF;
+        this.type = type;
+        this.id = ((Integer)next_id++).toString();
+    }
+    
+    public String getNameASCII() {
+        return nameASCII;
+    }
+    
+    public String getNameUTF() {
+        return nameUTF;
+    }
+    
+    public TypeToponym getType() {
+        return type;
+    }
+    
+    public String getId() {
+        return id;
+    }
 }
