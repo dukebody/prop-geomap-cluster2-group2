939dc4be7558ffa1db232df7f0e0a8f1ea8524dc
Israel Saeta Pérez <dukebody@gmail.com>
2011-11-20 15:21:00 +0100

    Toponyms controller and tests, unfinished.

---
+++ ToponymsController.java
@@ -0,0 +1,85 @@
+import java.util.*;
+
+
+class ToponymsController {
+    
+    private Trie<Toponym> toponymsTrie;
+    private Trie<TypeToponym> typeToponymsTrie;
+    private QuadTree<Double,Point> toponymsQuadTree;
+
+
+    public ToponymsController() {
+        toponymsTrie = new Trie<Toponym>();
+        typeToponymsTrie = new Trie<TypeToponym>();
+        toponymsQuadTree = new QuadTree<Double,Point>();
+    }
+
+    public boolean createTypeToponym(String name, String category, String code) {
+        TypeToponym typeToponym = new TypeToponym(name, category, code);
+        typeToponymsTrie.put(typeToponym, code);
+        return true;
+    }
+
+    public HashMap<String,String> getMap(Toponym toponym) {
+        HashMap<String,String> map = new HashMap<String,String>();
+        if (toponym != null) {
+            map.put("id", toponym.getId());
+            map.put("nameASCII", toponym.getNameASCII());
+            map.put("nameUTF", toponym.getNameUTF());
+            map.put("latitude", new Double(toponym.getLatitude()).toString());
+            map.put("longitude", new Double(toponym.getLongitude()).toString());
+            map.put("type", toponym.getType().getCode());
+            return map;
+        }
+
+        return null;
+    }
+
+    public List<HashMap<String,String>> getMap(List<Toponym> toponymList) {
+        List<HashMap<String,String>> mapList = new LinkedList<HashMap<String,String>>();
+        for (Toponym toponym: toponymList) {
+            mapList.add(getMap(toponym));
+        }
+
+        return mapList;
+    }
+
+    public boolean addToponym(String nameASCII, String nameUTF, double latitude, double longitude, String typeCode) {
+
+        // check that there doesn't exist already a toponym at the specified point
+        Interval<Double> intX = new Interval<Double>(latitude, latitude);
+        Interval<Double> intY = new Interval<Double>(longitude, longitude);
+        Interval2D<Double> rect = new Interval2D<Double>(intX, intY);
+        if (toponymsQuadTree.query2D(rect).isEmpty()) {
+            TypeToponym type = typeToponymsTrie.get(typeCode);
+            Toponym toponym = new Toponym(nameASCII, nameUTF, latitude, longitude, type);
+            toponymsTrie.put(toponym, nameUTF);
+            toponymsQuadTree.insert(latitude, longitude, toponym);
+            return true;
+        }
+        return false; // point already existent
+    }
+
+    public List<HashMap<String,String>> getToponymsByName(String name) {
+        return getMap(toponymsTrie.getList(name));
+    }
+
+    public HashMap<String,String> getToponymByNameAndId(String name, String id) {
+        return getMap(toponymsTrie.get(name, id));
+    }
+
+    public boolean modifyToponym(String nameUTF, String id, String newNameASCII, String newNameUTF, double newLatitude, 
+                                 double newLongitude, String newTypeCode) {
+        // erase old toponym from the quadtree and the trie
+        HashMap<String, String> oldToponym = getToponymByNameAndId(nameUTF, id);
+        if (oldToponym != null) {
+            System.out.println("chivato1");
+            toponymsTrie.remove(nameUTF, id);
+            toponymsQuadTree.remove(new Double(oldToponym.get("latitude")), new Double(oldToponym.get("longitude")));
+            addToponym(newNameASCII, newNameUTF, newLatitude, newLongitude, newTypeCode);
+            return true;
+        }
+        System.out.println("chivato2");
+        return false;  // original toponym not found
+    }
+}
---
+++ ToponymsControllerTest.java
@@ -0,0 +1,163 @@
+import java.util.*;
+
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+
+public class ToponymsControllerTest extends TestCase {
+
+    private ToponymsController tc;
+
+    @Before
+    public void setUp() throws Exception {
+        tc = new ToponymsController();
+
+        tc.createTypeToponym("City A", "City", "CA");
+        tc.createTypeToponym("City B", "City", "CB");
+
+        tc.addToponym("Madrid", "Madrid", 34.3332, 12.4333, "CA");
+        System.out.println(tc.getToponymsByName("Madrid"));
+        tc.addToponym("Barcelona", "Barcelona", 12.14, 1.2333, "CA");
+        tc.addToponym("Norilandia", "Ñorilandia", 345.2, 46.24, "CA");
+        tc.addToponym("Norilandia", "Norilandia", 346.2, 49.24, "CB");
+        tc.addToponym("Mandril", "Mandril", 45.14, 22.563, "CB");
+        tc.addToponym("Mandril", "Mandril", 45.18, 22.580, "CB");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    // public static junit.framework.Test suite() { 
+    //     return new JUnit4TestAdapter(ToponymsControllerTest.class); 
+    // }
+
+    @Test
+    public void testgetMap() {
+        HashMap<String,String> map = new HashMap();
+        map.put("id", "6"); map.put("nameASCII", "Madrid"); map.put("nameUTF", "Madrid"); map.put("latitude", "34.3332"); map.put("longitude", "12.4333");
+        map.put("type", "CA");
+        assertEquals(map, tc.getMap(new Toponym("Madrid", "Madrid", 34.3332, 12.4333, new TypeToponym("City A", "City", "CA"))));
+    }
+
+    @Test
+    public void testUniqueGetToponymsByName() {
+        System.out.println("Testing getToponymByName for unique names...");
+
+        List<HashMap<String,String>> toponyms = tc.getToponymsByName("Madrid");
+        System.out.print("\tGetting an unique existing toponym by ascii name should return a one-toponym list with that name...");
+        assertEquals("Madrid", toponyms.get(0).get("nameASCII"));
+        assertEquals(1, toponyms.size());
+        System.out.println("OK");
+
+        List<HashMap<String,String>> nonexistentToponym = tc.getToponymsByName("NonExistent");
+        System.out.print("\tGetting an non-existing toponym should return null...");
+        assertTrue(nonexistentToponym.isEmpty());
+        System.out.println("OK");
+    }
+
+    @Test
+    public void testSeveralGetToponymsByName() {
+        System.out.println("Testing getToponymByName for names matching more than one element...");
+
+        List<HashMap<String,String>> toponyms = tc.getToponymsByName("Mandril");
+        System.out.print("\tGetting an non-unique existing toponym by ascii name should return a list with the toponyms matching that name...");
+        assertEquals(2, toponyms.size());
+        System.out.println("OK");
+    }
+
+    @Test
+    public void testSeveralGetToponymByNameAndId() {
+        System.out.println("Testing getToponymByName for names matching more than one element...");
+
+        List<HashMap<String,String>> toponyms = tc.getToponymsByName("Mandril");
+        String secondId = toponyms.get(1).get("id");
+        HashMap<String,String> mandril = tc.getToponymByNameAndId("Mandril", secondId);
+        System.out.print("\tGetting an non-unique existing toponym by ascii name and id should return the toponym with that name and id...");
+        // The last inserted toponym is the last one by id in the associated Trie
+        assertEquals(new Double(45.18).toString(), mandril.get("latitude"));
+        System.out.println("OK");
+    }
+
+    @Test
+    public void testAddToponym() {
+        System.out.println("Testing addToponym...");
+
+        System.out.print("\tChecking that adding a toponym in a free location returns true...");
+        assertTrue(tc.addToponym("Zaragoza", "Zaragoza", 45.19, 22.580, "CA"));
+        System.out.println("OK");
+
+        HashMap<String,String> zaragoza = tc.getToponymsByName("Zaragoza").get(0);
+        System.out.print("\tChecking that it was really added...");
+        assertEquals("Zaragoza", zaragoza.get("nameASCII"));
+        System.out.println("OK");
+    }
+
+    @Test
+    public void testAddToponymOnExistingOneFails() {
+        System.out.println("Testing addToponym on top on an existing one (location-wise)...");
+
+        System.out.print("\tChecking that this returns false...");
+        assertFalse(tc.addToponym("Zaragoza", "Zaragoza", 45.14, 22.563, "CA"));
+        System.out.println("OK");
+
+        HashMap<String,String> mandril = tc.getToponymsByName("Mandril").get(0);
+        System.out.print("\tChecking that the old toponym remains at its place...");
+        assertEquals("45.14", mandril.get("latitude"));
+        System.out.println("OK");
+    }
+
+    @Test
+    public void testModifyToponym() {
+        System.out.println("Testing modifyToponym with an existing toponym...");
+
+        String id = tc.getToponymsByName("Madrid").get(0).get("id");
+        System.out.print("\tChecking that modifyToponym(valid_id, *valid_data) returns True...");
+        assertTrue(tc.modifyToponym("Madrid", id, "Almeria", "Almería", 33.2224, 24.5555, "CB"));
+        System.out.println("OK");
+        
+        System.out.print("\tChecking that the old toponym doesn't exist...");
+        assertTrue(tc.getToponymsByName("Madrid").isEmpty());
+        System.out.println("OK");
+        
+        System.out.print("\tChecking that the new toponym does exist...");
+        assertFalse(tc.getToponymsByName("Almeria").isEmpty());
+        System.out.println("OK");
+    }
+
+    @Test
+    public void testModifyNonExistingToponym() {
+        System.out.println("Testing modifyToponym for an unexisting toponym...");
+
+        System.out.print("\tChecking that modifyToponym(invalid_id, *valid_data) returns False...");
+        assertFalse(tc.modifyToponym("Madrid", "3000", "Almeria", "Almería", 33.2224, 24.5555, "CA"));
+        System.out.println("OK");
+
+        System.out.print("\tChecking that the old toponym still exists...");
+        assertFalse(tc.getToponymsByName("Madrid").isEmpty());
+        System.out.println("OK");
+
+        System.out.print("\tChecking that the new toponym doesn't exist...");
+        assertTrue(tc.getToponymsByName("Almeria").isEmpty());
+        System.out.println("OK");
+    }
+
+    @Test
+    public void testModifyExistingToponymWrongData() {
+        System.out.println("Testing modifyToponym with wrong data...");
+
+        String id = tc.getToponymsByName("Madrid").get(0).get("id");
+        System.out.print("\tChecking that modifyToponym(valid_id, *invalid_data) returns False...");
+        assertFalse(tc.modifyToponym("Madrid", id, "Almeria", "Almería", 6000.00, 43.2, "CB"));
+        System.out.println("OK");
+
+        System.out.print("\tChecking that the old toponym still exists...");
+        assertFalse(tc.getToponymsByName("Madrid").isEmpty());
+        System.out.println("OK");
+
+        System.out.print("\tChecking that the new toponym doesn't exist...");
+        assertTrue(tc.getToponymsByName("Almeria").isEmpty());
+        System.out.println("OK");
+    }
+}
