f8bd1f5fa0af39a3624fb1bff7a50c51adf560e1
Israel Saeta Pérez <dukebody@gmail.com>
2011-12-14 12:10:18 +0100

    Optimize the ToponymCode checking on creation.

--- CitiesDeserializer.java
+++ CitiesDeserializer.java
@@ -12,6 +12,13 @@ class CitiesDeserializer implements IDeserializer {
     public CitiesDeserializer(Iterator<HashMap<String,String>> itr, CitiesController cc) {
         this.itr = itr;
         this.cc = cc;
+
+        // hashset for fast searching
+        cityToponymCodes = new HashSet<String>();
+        Iterator<HashMap<String,String>> ttItr = cc.getToponymTypesIterator();
+        while (ttItr.hasNext()) {
+            cityToponymCodes.add(ttItr.next().get("CodiToponim"));
+        }
     }
 
     public void generate() {
@@ -19,8 +26,8 @@ class CitiesDeserializer implements IDeserializer {
             HashMap<String,String> currentMap = itr.next();
 
             // create only cities with population > 0
-            if (cc.getToponymType(currentMap.get("CodiToponim")) != null && !currentMap.get("Poblacio").equals("0")) {
-                boolean result = cc.addCity(currentMap.get("Nom_ASCII").replaceAll("_", " "), 
+            if (cityToponymCodes.contains(currentMap.get("CodiToponim")) && !currentMap.get("Poblacio").equals("0")) {
+                cc.addCity(currentMap.get("Nom_ASCII").replaceAll("_", " "), 
                     currentMap.get("Nom_UTF").replaceAll("_", " "), 
                     new Double(currentMap.get("Latitud")), 
                     new Double(currentMap.get("Longitud")),
