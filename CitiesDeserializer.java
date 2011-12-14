64263101084beedf123af3cfaf842cdc0b095978
Israel Saeta Pérez <dukebody@gmail.com>
2011-12-14 11:21:41 +0100

    Replace _ with spaces in citynames and discard cities without population.

--- CitiesDeserializer.java
+++ CitiesDeserializer.java
@@ -18,11 +18,14 @@ class CitiesDeserializer implements IDeserializer {
         while (itr.hasNext()) {
             HashMap<String,String> currentMap = itr.next();
 
-            // create only cities
-            if (cc.getToponymType(currentMap.get("CodiToponim")) != null) {
-                boolean result = cc.addCity(currentMap.get("Nom_ASCII"), currentMap.get("Nom_UTF"), 
-                    new Double(currentMap.get("Latitud")), new Double(currentMap.get("Longitud")),
-                    currentMap.get("CodiToponim"), new Integer(currentMap.get("Poblacio")));
+            // create only cities with population > 0
+            if (cc.getToponymType(currentMap.get("CodiToponim")) != null && !currentMap.get("Poblacio").equals("0")) {
+                boolean result = cc.addCity(currentMap.get("Nom_ASCII").replaceAll("_", " "), 
+                    currentMap.get("Nom_UTF").replaceAll("_", " "), 
+                    new Double(currentMap.get("Latitud")), 
+                    new Double(currentMap.get("Longitud")),
+                    currentMap.get("CodiToponim"), 
+                    new Integer(currentMap.get("Poblacio")));
             }
         }
     }
