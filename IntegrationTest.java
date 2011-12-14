a0e0f2432d39261bdbff0e2bda40f9222a9a2cd9
Israel Saeta Pérez <dukebody@gmail.com>
2011-12-14 09:28:58 +0100

    Integration tests skeleton.

---
+++ IntegrationTest.java
@@ -0,0 +1,103 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.textui.TestRunner;
+
+
+public class IntegrationTest extends TestCase {
+
+    public static void main(String[] args) {
+        TestSuite suite = new TestSuite();
+        TestRunner runner = new TestRunner();
+        suite.addTestSuite(IntegrationTest.class);
+        runner.doRun(suite);
+    }
+
+    private CitiesDeserializer cd;
+    private BorderPointsDeserializer bpd;
+    private ToponymTypesDeserializer ttd;
+
+    private CountryController countryc;
+    private ZonesController zonesc;
+    private CitiesController citiesc;
+
+    @Before
+    public void setUp() throws Exception {
+
+        countryc = new CountryController();
+        zonesc = new ZonesController(countryc.getBorderPointsQuadTree());
+        citiesc = new CitiesController();
+
+        File fFronteres = new File("Fronteres_A.txt");
+        BordersFileParser parserFronteres = new BordersFileParser(fFronteres);
+        Iterator<HashMap<String,String>> itrFronteres = parserFronteres.getIterator();
+
+
+        File fTypes = new File("FeatureCodes_Cities.txt");
+        TypesFileParser typesParser = new TypesFileParser(fTypes);
+        Iterator<HashMap<String,String>> itrTypes = typesParser.getIterator();
+        
+
+        File fToponyms = new File("Toponims10000.txt");
+        ToponymsFileParser parserToponyms = new ToponymsFileParser(fToponyms);
+        Iterator<HashMap<String,String>> itrToponyms = parserToponyms.getIterator();
+
+        cd = new CitiesDeserializer(itrToponyms, citiesc);
+        bpd = new BorderPointsDeserializer(itrFronteres, countryc, zonesc);
+        ttd = new ToponymTypesDeserializer(itrTypes, citiesc);
+
+        //ttd.generate();
+        bpd.generate();
+        //cd.generate();
+    }
+
+    // @Test
+    // public void testNToponymTypes() {
+    //     Iterator<HashMap<String,String>> itr = citiesc.getToponymTypesIterator();
+    //     int n = 0;
+    //     while (itr.hasNext()) {
+    //         itr.next();
+    //         n++;
+    //     }
+    //     System.out.print("Checking that there are 15 city-like toponym types...");
+    //     assertEquals(15, n);
+    //     System.out.println("OK");
+    // }
+
+    // @Test
+    // public void testGetNeighborCountries() {
+    // 	List<HashMap<String,String>> neighborCountries = countryc.getNeighborCountries("Spain");
+    // 	HashSet<String> countryNames = new HashSet<String>();
+    // 	for (HashMap<String,String> map: neighborCountries) {
+    // 		countryNames.add(map.get("name"));
+    // 	}
+    // 	System.out.print("Checking that Spain limits with France and Portugal...");
+    // 	System.out.print(countryNames);
+    // 	System.out.print(neighborCountries);
+    //     assertTrue(countryNames.contains("France"));
+    //     assertTrue(countryNames.contains("Portugal"));
+    //     System.out.println("OK");
+    // }
+
+	@Test
+    public void testTotalCoastLineLength() {
+    	System.out.print("Checking that Aruba has coast...");
+        assertEquals(25.0, countryc.getTotalCoastlineLength("Aruba"));
+        System.out.println("OK");
+    }
+
+    @Test
+    public void testTotalSharedBorderLength() {
+    	System.out.print("Checking that Aruba has coast...");
+        assertEquals(25.0, countryc.getTotalSharedBorderLength("Aruba"));
+        System.out.println("OK");
+    }
+
+}
