8133e490ce083db3fba5db511a96a1d9c362cd59
Israel Saeta Pérez <dukebody@gmail.com>
2011-12-15 13:15:59 +0100

    New MinHeap implementation with tests.

--- MinHeap.java
+++ MinHeap.java
@@ -1,78 +1,195 @@
-public class MinHeap {
-    private int[] Heap;
-    private int maxsize;
-    private int size;
+import java.util.*;
 
-    public MinHeap(int max) {
-        maxsize = max;
-        Heap = new int[maxsize];
-        size = 0 ;
-        Heap[0] = Integer.MIN_VALUE;
-    }
+/**
+   Taken from:
+   http://comscigate.com/HW/cs31/src_code/ch21/pqueue/MinHeap.java
+   We consider a Heap a "basic" data structure. :)
+*/
+public class MinHeap
+{
+   /**
+      Constructs an empty heap.
+   */
+   public MinHeap()
+   {
+      elements = new ArrayList<Comparable>();
+      elements.add(null); 
+   }
 
-    private int leftchild(int pos) {
-        return 2*pos;
-    }
-    private int rightchild(int pos) {
-        return 2*pos + 1;
-    }
+   /**
+      Adds a new element to this heap.
+      @param newElement the element to add
+   */
+   public void add(Comparable newElement)
+   {
+      // Add a new leaf
+      elements.add(null);
+      int index = elements.size() - 1;
+      
+      // Demote parents that are larger than the new element
+      while (index > 1 
+            && getParent(index).compareTo(newElement) > 0) 
+      {
+         elements.set(index, getParent(index));
+         index = getParentIndex(index);
+      }
 
-    private int parent(int pos) {
-        return  pos / 2;
-    }
-    
-    private boolean isleaf(int pos) {
-        return ((pos > size/2) && (pos <= size));
-    }
+      // Store the new element into the vacant slot
+      elements.set(index, newElement);
+   }
 
-    private void swap(int pos1, int pos2) {
-        int tmp;
+   /**
+      Gets the minimum element stored in this heap.
+      @return the minimum element
+   */
+   public Comparable peek()
+   {
+      return elements.get(1);
+   }
 
-        tmp = Heap[pos1];
-        Heap[pos1] = Heap[pos2];
-        Heap[pos2] = tmp;
-    }
+   /**
+      Removes the minimum element from this heap.
+      @return the minimum element
+   */
+   public Comparable remove()
+   {
+      Comparable minimum = elements.get(1);      
 
-    public void insert(int elem) {
-        size++;
-        Heap[size] = elem;
-        int current = size;
-        
-        while (Heap[current] < Heap[parent(current)]) {
-            swap(current, parent(current));
-            current = parent(current);
-        }   
-    }
+      // Remove last element
+      int lastIndex = elements.size() - 1;
+      Comparable last = elements.remove(lastIndex);
 
-    public void print() {
-        int i;
-        for (i=1; i<=size;i++)
-            System.out.print(Heap[i] + " ");
-        System.out.println();
-    }
+      if (lastIndex > 1)
+      {
+         elements.set(1, last);
+         fixHeap();     
+      }
 
-    public int removemin() {
-        swap(1,size);
-        size--;
-        if (size != 0)
-            pushdown(1);
-        return Heap[size+1];
-    }
+      return minimum;
+   }
 
-    private void pushdown(int position) {
-        int smallestchild;
-        while (!isleaf(position)) {
-            smallestchild = leftchild(position);
-            if ((smallestchild < size) && (Heap[smallestchild] > Heap[smallestchild+1]))
-            smallestchild = smallestchild + 1;
-            if (Heap[position] <= Heap[smallestchild]) return;
-            swap(position,smallestchild);
-            position = smallestchild;
-        }
-    }
+   /**
+      Turns the tree back into a heap, provided only the root 
+      node violates the heap condition.
+   */
+   private void fixHeap()
+   {
+      Comparable root = elements.get(1);
 
+      int lastIndex = elements.size() - 1;
+      // Promote children of removed root while they are larger than last      
+
+      int index = 1;
+      boolean more = true;
+      while (more)
+      {
+         int childIndex = getLeftChildIndex(index);
+         if (childIndex <= lastIndex)
+         {
+            // Get smaller child 
+
+            // Get left child first
+            Comparable child = getLeftChild(index);
+
+            // Use right child instead if it is smaller
+            if (getRightChildIndex(index) <= lastIndex 
+                  && getRightChild(index).compareTo(child) < 0)
+            {
+               childIndex = getRightChildIndex(index);
+               child = getRightChild(index);
+            }
+
+            // Check if larger child is smaller than root
+            if (child.compareTo(root) < 0) 
+            {
+               // Promote child
+               elements.set(index, child);
+               index = childIndex;
+            }
+            else
+            {
+               // Root is smaller than both children
+               more = false;
+            }
+         }
+         else 
+         {
+            // No children
+            more = false; 
+         }
+      }
+
+      // Store root element in vacant slot
+      elements.set(index, root);
+   }
+
+   /**
+      Returns the number of elements in this heap.
+   */
+   public int size()
+   {
+      return elements.size() - 1;
+   }
+
+   /**
+      Returns the index of the left child.
+      @param index the index of a node in this heap
+      @return the index of the left child of the given node
+   */
+   private static int getLeftChildIndex(int index)
+   {
+      return 2 * index;
+   }
+
+   /**
+      Returns the index of the right child.
+      @param index the index of a node in this heap
+      @return the index of the right child of the given node
+   */
+   private static int getRightChildIndex(int index)
+   {
+      return 2 * index + 1;
+   }
+
+   /**
+      Returns the index of the parent.
+      @param index the index of a node in this heap
+      @return the index of the parent of the given node
+   */
+   private static int getParentIndex(int index)
+   {
+      return index / 2;
+   }
+
+   /**
+      Returns the value of the left child.
+      @param index the index of a node in this heap
+      @return the value of the left child of the given node
+   */
+   private Comparable getLeftChild(int index)
+   {
+      return elements.get(2 * index);
+   }
+
+   /**
+      Returns the value of the right child.
+      @param index the index of a node in this heap
+      @return the value of the right child of the given node
+   */
+   private Comparable getRightChild(int index)
+   {
+      return elements.get(2 * index + 1);
+   }
+
+   /**
+      Returns the value of the parent.
+      @param index the index of a node in this heap
+      @return the value of the parent of the given node
+   */
+   private Comparable getParent(int index)
+   {
+      return elements.get(index / 2);
+   }
+
+   private ArrayList<Comparable> elements;
 }
-
-
-
-
---
+++ MinHeapTest.java
@@ -0,0 +1,55 @@
+import junit.framework.TestCase;
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MinHeapTest extends TestCase {
+
+    MinHeap mh;
+
+    @Before
+    public void setUp() throws Exception {
+        mh = new MinHeap();
+        mh.add(6);
+        mh.add(4);
+        mh.add(3);
+        mh.add(88);
+        mh.add(34);
+        mh.add(96);
+        mh.add(34);
+        mh.add(53);
+        mh.add(73);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testPeek() {
+        System.out.print("Checking that peek() returns the smallest element...");
+        assertEquals(3, mh.peek());
+        System.out.println("OK");
+    }
+
+    @Test
+    public void testRemove() {
+        System.out.print("Checking that remove() returns the smallest element...");
+        assertEquals(3, mh.remove());
+        System.out.println("OK");
+
+        System.out.print("Checking that after remove() the next smallest element is the expected one...");
+        assertEquals(4, mh.peek());
+        System.out.println("OK");
+    }
+
+    @Test
+    public void testSize() {
+        System.out.print("Checking that size() returns the correct number of elements in the heap...");
+        assertEquals(9, mh.size());
+        System.out.println("OK");
+    }
+}
