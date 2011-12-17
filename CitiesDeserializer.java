8f4914780eb4559dfdbde80ec20072e369603ed4
Israel Saeta Pérez <dukebody@gmail.com>
2011-12-17 23:34:58 +0100

    Javadocs for the cities deserializer.

--- CitiesDeserializer.java
+++ CitiesDeserializer.java
@@ -1,19 +1,41 @@
 import java.lang.*;
 import java.util.*;
 
+/**
+Component to transform the output of the ToponymsFileParser into the
+proper cities.
 
+Note that this deserializer should be used *after* importing the borderpoints
+and the toponymtypes into the data storage. Otherwise the cities might not be
+assigned to the right zones or not imported at all (due to toponym 
+type restrictions).
+*/
 class CitiesDeserializer implements IDeserializer {
     
     private Iterator<HashMap<String,String>> itr;
     private CitiesController cc;
 
     private static Set<String> cityToponymCodes;
-    
+
+    /**
+    Prepare a cities deserializer for the toponyms HashMap representation
+    coming from the specified iterator.
+
+    @param itr Iterator of a BorderPointsFileParser.
+
+    @param cc CitiesController to handle the city creation.
+    */
     public CitiesDeserializer(Iterator<HashMap<String,String>> itr, CitiesController cc) {
         this.itr = itr;
         this.cc = cc;
     }
 
+    /**
+    Create all the cities coming from the iterator.
+
+    Cities of a type different from the currently loaded ones and cities
+    with 0 population will be dropped.
+    */
     public void generate() {
         // hashset for fast searching
         cityToponymCodes = new HashSet<String>();
