056528828f7cdb45d60e163fb6bae555d9d82664
Israel Saeta Pérez <dukebody@gmail.com>
2011-12-17 20:31:28 +0100

    Javadoc for Country

--- BorderPointsDeserializer.java
+++ BorderPointsDeserializer.java
@@ -22,39 +22,40 @@ class BorderPointsDeserializer implements IDeserializer {
         Zone curr_Zone = null;
         String curr_ZoneId = "INVALID_ZONEID";
 
+        ArrayList<BorderPoint> zp = new ArrayList<BorderPoint>();
+        HashMap<String,String> currentMap;
 
         while (itr.hasNext()) {
             // get next map
-            HashMap<String,String> currentMap = itr.next();
+            currentMap = itr.next();
 
-            // if the country is different from the current one and does not exist, create it
-            if (!curr_CountryName.equals(currentMap.get("name_country"))) {
-                cc.addCountry(currentMap.get("name_country"), currentMap.get("id_country"));
-            }
-
-
-            curr_CountryName = currentMap.get("name_country");
-
-            // if the zone id is different from the current one, create a new zone for the current country
-            // and set the zone as current
+            // if the zone id is different from the current one, create the old zone for the old country
+            // and start storing new points for the new zone
             if (!currentMap.get("id_zone").equals(curr_ZoneId)) {
-                curr_Country = cc.getRawCountry(curr_CountryName);
-                ArrayList<BorderPoint> zp = new ArrayList<BorderPoint>();
+                if (!zp.isEmpty()) {
+                    zc.createZone(curr_Country, zp);
+                }
+                zp.clear();
 
-                zc.createZone(curr_Country, zp);
                 curr_ZoneId = currentMap.get("id_zone");
             }
 
-            curr_Zone = curr_Country.getZones().get(curr_Country.getZones().size()-1);
-
-            // add point to current country and zone
-            if (curr_Country != null && curr_Zone != null) {
-                
-                Double latitude = new Double(currentMap.get("latitude"));
-                Double longitude = new Double(currentMap.get("longitude"));
-                zc.addBorderPointZone(curr_Zone, new BorderPoint(latitude,longitude));
+            // if the country is different from the current one create it
+            // note that it won't be created if it already exists
+            if (!curr_CountryName.equals(currentMap.get("name_country"))) {
+                cc.addCountry(currentMap.get("name_country"), currentMap.get("id_country"));
+                curr_CountryName = currentMap.get("name_country");
+                curr_Country = cc.getRawCountry(curr_CountryName);
             }
 
+            // add current point to the list
+            Double latitude = new Double(currentMap.get("latitude"));
+            Double longitude = new Double(currentMap.get("longitude"));
+            zp.add(new BorderPoint(latitude,longitude));
+
         }
+
+        // create lastZone
+        zc.createZone(curr_Country, zp);
     }
 }
--- Country.java
+++ Country.java
@@ -1,11 +1,19 @@
 import java.util.*;
 
-class Country implements IGetId {
+/**
+   Basic data structure class for countries. Contains itss name and code.
+*/
+
+public class Country implements IGetId {
 
     private String name;
     private String code;
     private ArrayList<Zone> zones;
 
+    /**
+    Creates a country with the specified name.
+    @param name A string representing the name of the country.
+    */
     public Country(String name){
         this.name = name;
         this.zones = new ArrayList<Zone>();
